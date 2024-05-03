65da8ecbaee3e2d7e982474d2195b4f57fbd5c7c
ndtung25 <ndtung.dhkl17a2hn@sv.uneti.edu.vn>
2024-05-03 22:01:34 +0700

    nop btvn lab7

---
+++ lab7/bai5(1).py
@@ -0,0 +1,23 @@
+kho = {
+ "banana": 6,
+ "apple": 5,
+ "orange": 32,
+ "pear": 15
+}
+gia_tien = {
+ "banana": 4,
+ "apple": 2,
+ "orange": 1.5,
+ "pear": 3
+}
+hoa_don = 0
+for mat_hang, so_luong in kho.items():
+    don_gia = gia_tien[mat_hang]
+    thanh_tien = so_luong * don_gia
+    hoa_don += thanh_tien
+    print(f"Mặt hàng: {mat_hang}, Số lượng: {so_luong}, Đơn giá: {don_gia}, Thành tiền: {thanh_tien}")
+
+print(f"Tổng hóa đơn: {hoa_don}")
+
+for mat_hang, so_luong in kho.items():
+    print(f"Mặt hàng: {mat_hang}, Số lượng còn lại trong kho: {so_luong}")
